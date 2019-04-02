fed450ecc4d7113000322f37a69fd46ed23eb601
ypablo <yfedora@gmail.com>
2019-04-02 15:35:52 +0100

    Add all files

--- script-compiled.js
+++ script-compiled.js
@@ -1,6 +1,34 @@
-'use strict';
+"use strict";
 
-var sayHello = function sayHello() {
-  return alert('Hello world!');
+//Zadanie 1 
+var h = "Hello";
+var w = "World";
+
+var hw = h + " " + w + "!";
+console.log(hw);
+
+//Zadanie 2
+var multiply = function multiply(x) {
+  var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+  return x * y;
 };
-sayHello();
+console.log(multiply(5));
+
+//Zadanie 3
+var average = function average(arr) {
+  return arr.reduce(function (x, y) {
+    return x + y;
+  }, 0) / arr.length;
+};
+console.log(average([8, 25, 1, 30, 6, 20]));
+
+//Zadanie 4
+var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+console.log(average([].concat(grades)));
+
+//Zadanie 5
+var foo = [1, 4, 'Iwona', false, 'Nowak'];
+var x = foo[2],
+    y = foo[4];
+
+console.log(x + " " + y);
--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-const sayHello = () => alert('Hello world!');
-sayHello();
+//Zadanie 1 
+const h = "Hello";
+const w = "World";
+
+const hw = `${h} ${w}!`;
+console.log(hw);
+
+//Zadanie 2
+const multiply = (x, y = 1) => x*y;
+console.log(multiply(5));
+
+//Zadanie 3
+const average = arr => arr.reduce( ( x, y ) => x + y, 0 ) / arr.length;
+console.log(average( [ 8, 25, 1, 30, 6, 20 ] ));
+
+//Zadanie 4
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1]
+console.log(average([...grades]));
+
+//Zadanie 5
+const foo = [1, 4, 'Iwona', false, 'Nowak'];
+const [, ,x, ,y] = foo;
+console.log(`${x} ${y}`);
